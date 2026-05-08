1ba9cf70891071c28811dce9a5e3f0bba50392d2
supersbot6-eng <supersbot6-eng@github.com>
2026-05-08 11:09:43 -0400

    Commit shared theme tokens

---
+++ src/themeTokens.js
@@ -0,0 +1,11 @@
+export const colors = {
+  green: '#0f7a3f',
+  greenDark: '#0a5c2e',
+  greenLight: '#12924a',
+  yellow: '#d9b44a',
+  yellowLight: '#e5c56a',
+  offWhite: '#f5f5f0',
+  offWhiteMuted: '#e8e8e0',
+  textMuted: '#7a8a7a',
+  textDark: '#1a3d2a',
+};
